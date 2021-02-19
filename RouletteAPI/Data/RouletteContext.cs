d4b291f8d61bb066ed5513882b9fd7140908d25b
Jose Manrique Cruz <jmanriquecruz@gmail.com>
2021-02-18 23:39:31 -0500

    2021-02-18T23:21:24-05:00 Se agrega clase para el mapeo de entidades al modelo de apuestas

---
+++ RouletteAPI/Data/RouletteContext.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Data
+{
+    public class RouletteContext : IRouletteContext
+    {
+        private readonly ConnectionMultiplexer _redisConnection;
+        public RouletteContext(ConnectionMultiplexer redisConnection)
+        {
+            _redisConnection = redisConnection;
+            Redis = redisConnection.GetDatabase();
+        }
+
+        public IDatabase Redis { get; }
+    }
+}
---
+++ RouletteAPI/Mapping/RouletteMapping.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Mapping
+{
+    public class RouletteMapping : Profile
+    {
+        public RouletteMapping()
+        {
+            CreateMap<Models.Bet, Entities.Bet>().ReverseMap();
+        }
+        
+    }
+}
