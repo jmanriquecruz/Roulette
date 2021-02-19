2a821cf358d8ce2f07f2454098144339c1c0053f
Jose Manrique Cruz <jmanriquecruz@gmail.com>
2021-02-18 23:36:13 -0500

    2021-02-18T23:21:24-05:00 Creacion de entidades
    Creacion de entidades de la ruleta

---
+++ RouletteAPI/Entities/Lottery.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Entities
+{
+    public class Lottery
+    {
+        public DateTime RegisterDate{ get; set; }
+        public string RouletteId { get; set; }
+        public DateTime Open { get; set; }
+        public DateTime Close { get; set; }
+        public long TotalBet { get; set; }
+        public int WinningNumber { get; set; }
+        public int WinnersQuantityColor { get; set; }
+        public int WinnersQuantityNumber { get; set; }
+        public int TotalMoneyBetNumber { get; set; }
+        public int TotalMoneyBetColor { get; set; }
+        public List<Winner> WinnersNumber { get; set; }
+        public List<Winner> WinnersColor { get; set; }
+    }
+}
---
+++ RouletteAPI/Entities/Roulette.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Entities
+{
+    public class Roulette
+    {
+        public string RouletteId { get; set; }
+        public DateTime OpenDate {get;set;}
+        public DateTime CloseDate { get; set; }
+        public bool Status { get; set; }  
+        public List<Bet> Bets { get; set; }
+
+    }
+}
