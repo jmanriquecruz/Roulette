bbba2ba1c5c686d0d0beecedc87e179c2a16e3a3
Jose Manrique Cruz <jmanriquecruz@gmail.com>
2021-02-18 23:38:47 -0500

    2021-02-18T23:21:24-05:00 Se incluye entidad para las apuestas

---
+++ RouletteAPI/Entities/Bet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteAPI.Entities
+{
+    public class Bet
+    {
+        public int UserId { get; set; }
+        public int Number { get; set; }
+        public int MoneyBet { get; set; }
+    }
+}
